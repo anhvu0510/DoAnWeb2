259900180c70f70c19e0936b9e3c82149c7ca8ba
TranVanThuong <1760439@student.hcmus.edu.vn>
2020-07-31 15:10:50 +0700

    Create Payment Account + Create Saving Account

---
+++ Routes/account.route.js
@@ -0,0 +1,10 @@
+const Express = require('express');
+const Router = Express.Router();
+
+Router.get('/', (req, res) => {
+    res.render('PageAccount',{title : 'Account Managament'})
+})
+Router.use('/createpaymentaccount', require('./createpaymentaccount.route'));
+Router.use('/createsavingaccount', require('./createsavingaccount.route'));
+
+module.exports = Router;
