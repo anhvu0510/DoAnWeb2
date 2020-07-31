6b8d4dab026ca5745ed695f5e041eb063aa0bb95
poiutoth@gmail.com <poiutoth@gmail.com>
2020-08-01 02:08:16 +0700

    add update user's information

--- Routes/customer.route.js
+++ Routes/customer.route.js
@@ -1,6 +1,7 @@
 const Express = require("express");
 const Router = Express.Router();
 const User = require("../Services/user");
+const Customer = require("../Services/customer");
 
 const { authentication } = require("../Middleware/auth");
 
@@ -11,8 +12,68 @@ Router.get("/", (req, res) => {
 });
 
 // update user's information
-Router.get("/information", (req, res) => {
-  res.render("PageCustomerInformation", { title: "Customer Information" });
+Router.get("/information", async (req, res) => {
+  const curCustomer = await Customer.findByUserId(req.user.id);
+  res.render("PageCustomerInformation", {
+    title: "Customer Information",
+    name: curCustomer.name,
+    DOB: curCustomer.dob,
+    phone: curCustomer.phone,
+    address: curCustomer.address,
+    gender: curCustomer.gender,
+    identify: curCustomer.identify,
+    identifyID: curCustomer.identifyID,
+    identifyDate: curCustomer.identifyDate
+  });
+}).post("/information", async (req, res) => {
+  const curCustomer = await Customer.findByUserId(req.user.id);
+
+  //const { Name, DOB, Gender, Phone, Address, Identify, IdentifyID, IdentifyDate } = req.body;
+  const Name = req.body.name;
+  const DOB = req.body.DOB;
+  const Gender = req.body.gender;
+  const Phone = req.body.phone;
+  const Address = req.body.address;
+  const Identify = req.body.identify;
+  const IdentifyID = req.body.identifyID;
+  const IdentifyDate = req.body.identifyDate;
+
+  const isSuccess = await Customer.updateInformation(
+    curCustomer.id,
+    Name,
+    DOB,
+    Gender,
+    Phone,
+    Address
+  );
+
+  if (isSuccess[0] === 1) {
+    res.render("PageCustomerInformation", {
+      title: "Customer Information",
+      name: Name,
+      DOB: DOB,
+      phone: Phone,
+      address: Address,
+      gender: Gender,
+      identify: Identify,
+      identifyID: IdentifyID,
+      identifyDate: IdentifyDate,
+      success_msg: "Information updated successfully"
+    });
+  } else {
+    res.render("PageCustomerInformation", {
+      title: "Customer Information",
+      name: curCustomer.name,
+      DOB: curCustomer.dob,
+      phone: curCustomer.phone,
+      address: curCustomer.address,
+      gender: curCustomer.gender,
+      identify: curCustomer.identify,
+      identifyID: curCustomer.identifyID,
+      identifyDate: curCustomer.identifyDate,
+      error_msg: "Failed to update information, please try again"
+    });
+  }
 });
 
 // change user's password
--- Services/customer.js
+++ Services/customer.js
@@ -1,83 +1,105 @@
-const Sequelize = require('sequelize');
-const db = require('./db');
-const User = require('./user');
+const Sequelize = require("sequelize");
+const db = require("./db");
+const User = require("./user");
 
 const Model = Sequelize.Model;
 class Customer extends Model {
-    static async findById(user_id) {
-        return Customer.findByPk(user_id);
-    }
-    static async findByPhoneNumber(phone_number) {
-        return Customer.findOne({
-            where: { phone_number },
-        });
-    }
-    static async findByEmail(email) {
-        return Customer.findOne({
-            where: { email },
-        });
-    }
-    static async findByIdCard(id_card) {
-        return Customer.findOne({
-            where: { id_card },
-        });
-    }
+  static async findById(user_id) {
+    return Customer.findByPk(user_id);
+  }
+  static async findByPhoneNumber(phone_number) {
+    return await Customer.findOne({
+      where: { phone: phone_number }
+    });
+  }
+  static async findByIdentifyID(identify_id) {
+    return await Customer.findOne({
+      where: { identifyID: identify_id }
+    });
+  }
+  static async findByUserId(user_id) {
+    return await Customer.findOne({
+      where: { userId: user_id }
+    });
+  }
+  static async updateDate(date) {
+    return await Customer.update(
+      {
+        identifyDate: date
+      },
+      {
+        where: { id: "1" }
+      }
+    );
+  }
+  static async updateInformation(id, name, dob, gender, phone, address) {
+    return await Customer.update(
+      {
+        name: name,
+        dob: dob,
+        gender: gender,
+        phone: phone,
+        address: address
+      },
+      { where: { id: id } }
+    );
+  }
 }
 
 Customer.init(
-    {
-        id: {
-            type: Sequelize.STRING,
-            allowNull: false,
-            primaryKey: true,
-        },
-        name: {
-            allowNull: false,
-            type: Sequelize.STRING,
-        },
-        dob: {
-            allowNull: false,
-            type: Sequelize.DATEONLY,
-        },
-        gender: {
-            allowNull: false,
-            type: Sequelize.STRING,
-        },
-        phone: {
-            allowNull: false,
-            type: Sequelize.STRING,
-        },
-        address: {
-            allowNull: false,
-            type: Sequelize.STRING,
-        },
-        identify: {
-            allowNull: false,
-            type : Sequelize.STRING
-        },
-        identifyID: {
-            allowNull: false,
-            type: Sequelize.STRING,
-        },
-        identifyDate: {
-            allowNull: false,
-            type: Sequelize.DATEONLY,
-        },
-        identifyFontImg: {
-            allowNull: false,
-            type: Sequelize.STRING,
-        },
-        identifyEndImg: {
-            allowNull: false,
-            type: Sequelize.STRING,
-        },
+  {
+    id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true
     },
-    {
-        sequelize: db,
-        modelName: 'customer',
+    name: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    dob: {
+      allowNull: false,
+      type: Sequelize.DATEONLY
+    },
+    gender: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    phone: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    address: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    identify: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    identifyID: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    identifyDate: {
+      allowNull: false,
+      type: Sequelize.DATEONLY
+    },
+    identifyFontImg: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    identifyEndImg: {
+      allowNull: false,
+      type: Sequelize.STRING
     }
+  },
+  {
+    sequelize: db,
+    modelName: "customer"
+  }
 );
 
-User.hasOne(Customer)
-Customer.belongsTo(User)
+User.hasOne(Customer);
+Customer.belongsTo(User);
 module.exports = Customer;
